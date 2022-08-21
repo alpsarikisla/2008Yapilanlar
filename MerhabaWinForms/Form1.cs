7055e4eb9d96982ff2ee09a4dea27b33701ff506
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-08-21 14:15:57 +0300

    First Commit

---
+++ MerhabaWinForms/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MerhabaWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            TextBox tb_Selam = new TextBox();
+            tb_Selam.Size = new Size(250, 50);
+            tb_Selam.Location = new Point(100, 20);
+            this.Controls.Add(tb_Selam);
+        }
+    }
+}
